fcbb1cd5131a54413b95893b81d591a53896d81d
Алексей Писарев <afonasiu2983@gmail.com>
2022-10-07 12:40:50 +1000

    sozdanie graviheskix elementov

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,90 @@ namespace Wpf_UP4_PR_Pisarev
         {
             InitializeComponent();
         }
+
+        private void Win1_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+        }
+
+        private void Win2_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 window2 = new Window2();
+            window2.Show();
+        }
+
+        private void Win3_Click(object sender, RoutedEventArgs e)
+        {
+            Window3 window3 = new Window3();
+            window3.Show();
+        }
+
+        private void Win4_Click(object sender, RoutedEventArgs e)
+        {
+            Window4 window4 = new Window4();
+            window4.Show();
+        }
+
+        private void Win5_Click(object sender, RoutedEventArgs e)
+        {
+            Window5 window5 = new Window5();
+            window5.Show();
+        }
+
+        private void Win6_Click(object sender, RoutedEventArgs e)
+        {
+            Window6 window6 = new Window6();
+            window6.Show();
+        }
+        private void Win7_Click(object sender, RoutedEventArgs e)
+        {
+            Window7 window7 = new Window7();
+            window7.Show();
+        }
+        private void Win8_Click(object sender, RoutedEventArgs e)
+        {
+            Window8 window8 = new Window8();
+            window8.Show();
+        }
+        private void Win9_Click(object sender, RoutedEventArgs e)
+        {
+            Window9 window9 = new Window9();
+            window9.Show();
+        }
+        private void Win10_Click(object sender, RoutedEventArgs e)
+        {
+            Window10 window10 = new Window10();
+            window10.Show();
+        }
+
+        private void Win12_Click(object sender, RoutedEventArgs e)
+        {
+            Window12 window12 = new Window12();
+            window12.Show();
+        }
+
+        private void Win13_Click(object sender, RoutedEventArgs e)
+        {
+            Window13 window13 = new Window13();
+            window13.Show();
+        }
+
+        private void Win14_Click(object sender, RoutedEventArgs e)
+        {
+            Window14 window14 = new Window14();
+            window14.Show();
+        }
+
+        private void Win15_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Win16_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
-}
+    }
+
